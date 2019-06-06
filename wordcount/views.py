e28da87bc57082e42e8551af95944853d9bac785
do02reen24 <do02reen24@gmail.com>
2019-06-06 14:22:15 +0900

    add ex_count

--- wordcount/views.py
+++ wordcount/views.py
@@ -7,6 +7,9 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+def ex_count(request):
+    return render(request, 'ex_count.html')
+
 def result(request):
     text=request.GET['fulltext']
     words = text.split() # 공백 기준으로 나눠서 리스트로 만들어줌.
--- wordcount_project/urls.py
+++ wordcount_project/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('',wordcount.views.home,name="home"),
     path('about/',wordcount.views.about, name="about"),
     path('result/', wordcount.views.result, name="result"),
+    path('ex/',wordcount.views.ex_count,name="ex_count"),
 ]
